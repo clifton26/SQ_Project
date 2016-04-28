cc0dabea7025841958649dfe0182756536d5e14b
clifton26 <clifton.clunie26@gmail.com>
2016-04-28 12:59:34 +0100

    First commit

---
+++ src/test/java/ImdbTests.java
@@ -0,0 +1,54 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+/**
+ * Created by Clifton on 28/4/16.
+ */
+public class ImdbTests {
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    @Before
+    public void setUp() throws Exception {
+        driver = new HtmlUnitDriver();
+        baseUrl = "http://www.imdb.com/";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testImdbTests2() throws Exception {
+        driver.get(baseUrl + "/");
+        String searchTerm = "";
+        driver.findElement(By.id("navbar-query")).clear();
+        driver.findElement(By.id("navbar-query")).sendKeys(searchTerm);
+        driver.findElement(By.id("navbar-submit-button")).click();
+        driver.findElement(By.xpath("//td[2]/a")).click();
+        assertTrue(driver.getTitle().matches("^regexpi:\\.[\\s\\S]*\\$\\{searchTerm\\}\\.[\\s\\S]*$"));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+
+
+}
